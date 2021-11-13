ab94bc70a9dd2789fbfbdb87d4d50928897e852e
AnjaliJain-17 <anjali.wie@gmail.com>
2021-11-13 00:07:30 -0800

    update curl-users.

--- php/curl-users.php
+++ php/curl-users.php
@@ -1,13 +1,21 @@
 
-<p>Users registered in All Things Veg! : </p>      	
-    
-    <?php
-        $ch = curl_init("https://madhurimadani.com/allThingsVegUsers.php");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $userArr = explode(',', $result); 
-        foreach ($userArr as $value) {
-            echo "$value <br>";
-        }
-    ?>
+<p>All Users list! : </p>      	
+<?php
+
+$curl_handle = curl_init();
+curl_setopt($curl_handle, CURLOPT_URL, "http://anjalisjain.me/php/all-users.php");
+curl_setopt($curl_handle, CURLOPT_HEADER, 0);
+curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
+$contents = curl_exec($curl_handle);
+curl_setopt($curl_handle, CURLOPT_URL, "https://madhurimadani.com/allThingsVegUsers.php");
+$contents = $contents.",".curl_exec($curl_handle);
+
+
+#echo "<br/>";
+curl_close($curl_handle);
+
+foreach (explode(",",$contents) as $value) {
+    echo $value."<br/>";
+}
+
+?>
